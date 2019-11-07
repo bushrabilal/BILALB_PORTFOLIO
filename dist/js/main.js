e5f19bade8cdcaaa9c59fc23fbcdf21c07b748e1
Bushra Bilal <bushrabilal@gmail.com>
2019-11-07 13:02:24 -0500

    Menu Button, JavaScript, Rotate

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,21 +1,21 @@
 //select DOM Items
 const menuBtn = document.querySelector(".menu-btn");
-const menu = document.querySelector("menu");
+const menu = document.querySelector(".menu");
 const menuNav = document.querySelector(".menu-nav");
-const menuBranding = document.querySelector(".menu-brandings");
-const navItems = document.querySelectorAll(".nav-navItems");
+const menuBranding = document.querySelector(".menu-branding");
+const navItem = document.querySelectorAll(".nav-navItem");
 
-//set initial state of Men
+//set initial state of Menu
 let showMenu = false;
 
-menuBtn.addEventListener(click, toggleMenu);
+menuBtn.addEventListener("click", toggleMenu);
 function toggleMenu() {
   if (!showMenu) {
     menuBtn.classList.add("close");
     menu.classList.add("show");
     menuNav.classList.add("show");
     menuBranding.classList.add("show");
-    navItems.forEach(item => item.classList.add("show"));
+    navItem.forEach(item => item.classList.add("show"));
 
     //set Menu State
     showMenu = true;
@@ -24,7 +24,7 @@ function toggleMenu() {
     menu.classList.remove("show");
     menuNav.classList.remove("show");
     menuBranding.classList.remove("show");
-    navItems.forEach(item => item.classList.remove("show"));
+    navItem.forEach(item => item.classList.remove("show"));
     //set Menu State
     showMenu = false;
   }
